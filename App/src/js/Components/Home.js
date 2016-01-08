9f94c09732aab31b64194706305f5dc74ce9e486
shrey230195 <shreydixit93@gmail.com>
2016-01-09 05:11:28 +0530

    finished

---
+++ App/src/js/Components/Home.js
@@ -0,0 +1,47 @@
+/** 
+
+ * @jsx React.DOM 
+ */
+
+
+var React = require('React');
+var Address=require('./Address');
+var _ = require('lodash-node');
+
+var Home= React.createClass({
+	getInitialState:function(){
+		return{
+		Addresses:this.props.Addresses
+		}
+	},
+	 deleteAddress:function(Address){
+	 	var newAddress = _.without(this.state.Addresses,Address);
+	 	this.setState({ 
+	 		Addresses:newAddress
+	 		});
+	 },
+
+	render:function(){
+		var Addresses = this.state.Addresses.map(function(Add){
+
+				return <Address Address={Add} onDelete={this.deleteAddress}/>
+			}.bind(this));
+		return(
+			
+			<div className="jumbotron">
+
+				<img className="img-circle" src="src/images/MyPhoto.jpg"/>
+				
+
+				<h1> Shrey </h1>
+				<h3> Address </h3>
+				{Addresses}
+
+
+				
+			</div>
+			);
+	}
+});
+
+module.exports=Home;
